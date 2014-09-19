52f97f9530bccd463aedc82c536d470fdca24e7b
Jia-Shen Boon <jiashen@gmail.com>
2014-09-18 19:51:54 -0500

    Commit tree implementation

---
+++ Node.py
@@ -0,0 +1,11 @@
+# Implements a tree
+# A node has attribute childen as a list (empty by default) and attribute
+# data
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append((child_node)
